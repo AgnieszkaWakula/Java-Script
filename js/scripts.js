6162f0e588e6da581fdf39b8a1a128d6997f85f8
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-06-19 23:30:53 +0200

    tablice

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,17 @@ console.log(getTriangleArea(-10,6));
 var triangle1Area = getTriangleArea(10,15);
 var triangle2Area = getTriangleArea(10,4);
 var triangle3Area = getTriangleArea(10,2);
+
+
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName)===-1) {
+	var newName = allNames.push('Marian');
+}
+console.log(allNames);
