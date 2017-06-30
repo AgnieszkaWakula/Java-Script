689972edeee9c84887f8a8845413d86f62827de8
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-06-30 23:04:37 +0200

    OOP

--- js/smartphone.js
+++ js/smartphone.js
@@ -8,7 +8,7 @@ function Phone(brand, price, color, system, RAM) {
 	this.RAM = RAM
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "PLN" + "System phone is " + this.system + " memory RAM is " + this.RAM + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "PLN" + " System phone is " + this.system + " memory RAM is " + this.RAM + ".");
 }
 
 Phone.prototype.addVat = function (value) {
@@ -17,17 +17,17 @@ Phone.prototype.addVat = function (value) {
 
 var iPhone6S = new Phone("Apple", 2250, "silver", "iOS 10", "1 GB");
 var SamsungGalaxyS6 = new Phone ("Samsung", 1600, "gold", "Android", "3 GB");
-var OnePlusOne = ("OnePlus", 1299, "black", "Cyanogen OS 11S", "3 GB");
+var OnePlusOne = new Phone ("OnePlus", 1299, "black", "Cyanogen OS 11S", "3 GB");
 
 iPhone6S.printInfo();
 SamsungGalaxyS6.printInfo();
 OnePlusOne.printInfo();
 
-var galaxyWithVat = galaxyS6.addVat(23);
+var galaxyWithVat = SamsungGalaxyS6.addVat(23);
 	console.log(galaxyWithVat + " PLN");
 var iPhoneWithVat = iPhone6S.addVat(23);
 	console.log(iPhoneWithVat + " PLN");
-var onePlusWithVat = onePlusOne.addVat(23);
+var onePlusWithVat = OnePlusOne.addVat(23);
 	console.log(onePlusWithVat + " PLN");
 
 /*
