161d9a2d81b7c1203d2e9dfaade9ed04c402494c
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-07-01 22:38:11 +0200

    użycie Self

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 // scripts.js
 
-function Button() {
+function Button(text) {
 	this.text = text || 'Hello';
 }
 
 Button.prototype = {
 	create: function() {
-	var self = this;
-	this.$element = $('<button>');
-	this.$element.text(this.text);
-	this.$element.click(function() {
-		alert(self.text);
-	});
-	//$('body').append(this.$element);
-	this.$element.appendTo($('body'));
+		var self = this;
+
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+
+		this.$element.click(function() {
+			alert(self.text);
+		});
+
+		//this.$element.appendTo($('body'));
+		$('body').append(this.$element);
 	}
-}
+};
 
 var btn1 = new Button('Hello!');
 btn1.create();
